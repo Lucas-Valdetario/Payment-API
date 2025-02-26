872b8407f09c1bdc8211d62edf6b61f08be2324d
Lucas Valdetario Vieira <lucas.vieira@embrapii.org.br>
2025-02-26 15:46:27 -0300

    Updating

--- tests/test_pix.py
+++ tests/test_pix.py
@@ -3,7 +3,7 @@ sys.path.append("../")
 
 import pytest
 import os
-from payments import Pix
+from payments.pix import Pix
 
 def test_pix_create_payment():
     pix_instance = Pix()
@@ -11,4 +11,8 @@ def test_pix_create_payment():
     #create a payment
     payment_info = pix_instance.create_payment(base_dir="../")
 
-    assert "bank_payment_id" in payment_info
+    assert "bank_payment_id" in payment_info 
+    assert "qr_code_path" in payment_info
+
+    qr_code_path = payment_info["qr_code_path"]
+    assert os.path.isfile(f"../static/img/{qr_code_path}.png")
